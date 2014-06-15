2fc954ec34af0c5b6b0cba4b8240a55d4a300aa7
hughrawlinson <hugh@codeoclock.net>
2014-06-15 08:07:17 +0100

    we have bass

--- public/javascripts/Lead.js
+++ public/javascripts/Lead.js
@@ -1,29 +1,27 @@
 'use strict';
 
 define(["MelodySequencer"],function(MelodySequencer){
-    function Lead(context, destination=context.destination, type = "triangle"){
-        console.log("Lead Instantiated");
+    function Lead(context, bass=false, destination=context.destination, type = "triangle"){
         this.context = context;
         this.osc = context.createOscillator();
         this.lpf = context.createBiquadFilter();
         this.gain = context.createGain();
-        this.lpf.frequency.value = 3000;
-        this.lpf.Q.value = 20;
+        this.lpf.frequency.value = bass?600:1500;
+        this.lpf.Q.value = bass?35:20;
         this.lpf.type.value = "lowpass";
         this.lpf.gain.value = 1;
-        this.osc.type = type;
+        this.osc.type = bass?"sawtooth":type;
         this.osc.connect(this.lpf);
         this.gain.value = 0;
         this.lpf.connect(this.gain);
         this.gain.connect(destination)
         this.osc.start(0);
-        var impulse = function(freq){
-            console.log('this happens');
-            this.osc.frequency.value = freq;
-            this.gain.gain.linearRampToValueAtTime(1, this.context.currentTime+0.01);
-            this.gain.gain.linearRampToValueAtTime(0, this.context.currentTime+0.5);
-        }
-        this.melseq = new MelodySequencer(impulse);
+        var controller = this;
+        this.melseq = new MelodySequencer(bass?-2:0,function(freq){
+            controller.osc.frequency.value = freq;
+            controller.gain.gain.linearRampToValueAtTime(1, controller.context.currentTime+0.01);
+            controller.gain.gain.linearRampToValueAtTime(0, controller.context.currentTime+0.5);
+        });
     }
     return Lead;
 });
